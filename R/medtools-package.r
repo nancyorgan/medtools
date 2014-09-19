d27348048b722c1599a80fe7fb640cd487ae630d
nancyorgan <nancy.organ@vanderbilt.edu>
2014-09-19 11:15:42 -0500

    this
    this, okay????

--- R/medtools-package.r
+++ R/medtools-package.r
@@ -3,9 +3,5 @@
 #' @docType package
 #' @description Medical calculators for use in clinical research. 
 #' @author Nancy Organ
-#' @usage 
-#' cvsofa(volume = 10, conc = 32, kg = 50)
-#' mapcalc(systolic = 120, diastolic = 80)
-#' SF.ratio(o2sat = 90, fio2 = 0.4, liter = 8)
 #' 
 NULL
