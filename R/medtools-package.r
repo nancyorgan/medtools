26968a61a2b8c8420f484e3e10e0b42912999e58
nancyorgan <nancy.organ@vanderbilt.edu>
2014-09-19 11:11:37 -0500

    fix package description??!?!?!?
    fix package description??!?!?!?

--- R/medtools-package.r
+++ R/medtools-package.r
@@ -3,8 +3,9 @@
 #' @docType package
 #' @description Medical calculators for use in clinical research. 
 #' @author Nancy Organ
-#' @usage cvsofa(volume = 10, conc = 32, kg = 50)
-#' @usage mapcalc(systolic = 120, diastolic = 80)
-#' @usage SF.ratio(o2sat = 90, fio2 = 0.4, liter = 8)
+#' @usage 
+#' cvsofa(volume = 10, conc = 32, kg = 50)
+#' mapcalc(systolic = 120, diastolic = 80)
+#' SF.ratio(o2sat = 90, fio2 = 0.4, liter = 8)
 #' 
 NULL
